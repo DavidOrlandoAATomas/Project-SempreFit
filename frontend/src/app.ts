07d5dbfa3c0bb2e7cdee1b2296b71176f065957f
DavidOrlandoAATomas <darktomas2045@gmail.com>
2026-06-30 16:07:26 +0100

    Update API base URL to use environment variable

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3001/api"
+  baseURL:  process.env.NEXT_PUBLIC_API_URL || "http://10.0.0.10:3002/api";
 });
 
 api.interceptors.request.use(
